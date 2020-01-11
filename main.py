ee904bae2536b124540be49be12a223877f438eb
EatingSugar <whiteleya-y15@mgs.org>
2020-01-11 19:32:23 +0000

    Url Gen V1

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def urlgen():
   brand = brand.lower()
   code = input("product code: ")
   size = int(input("size: "))
+  while size.isdigit() == False:
+   size = int(input("size: ")) 
   url = "https://www.adidas.co.uk/" + brand + "-shoes/" + code + ".html?forceSelSize=" + code + "_" + str(460 + (size * 20))
   print(url)
 
