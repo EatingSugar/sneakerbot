51e4aaff407ccc9c8918b1a970c32068545f3bd0
EatingSugar <whiteleya-y15@mgs.org>
2020-01-11 19:28:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# url gen
+def urlgen():
+  brand = input("brand: ")
+  brand = brand.replace(" ", "-")
+  brand = brand.lower()
+  code = input("product code: ")
+  size = int(input("size: "))
+  url = "https://www.adidas.co.uk/" + brand + "-shoes/" + code + ".html?forceSelSize=" + code + "_" + str(460 + (size * 20))
+  print(url)
+
+urlgen()
